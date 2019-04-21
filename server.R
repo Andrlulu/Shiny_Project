bd65f618b98a384e2355cc424eebb4c54fc13e68
weijianguo <weijianguo0303@gmail.com>
2019-04-20 23:35:10 -0400

    Fixed slow loading, add some plots

--- server.R
+++ server.R
@@ -1,147 +1,96 @@
 
 
-server = shinyServer(
+shinyServer(
   
   function(input,output, session){
-    
+    r_time_rating <- reactive(
+      data %>% 
+        mutate(work.balance.stars = as.numeric(work.balance.stars),
+               culture.values.stars = as.numeric(culture.values.stars),
+               carrer.opportunities.stars = as.numeric(carrer.opportunities.stars),
+               comp.benefit.stars = as.numeric(comp.benefit.stars),
+               senior.mangemnet.stars = as.numeric(comp.benefit.stars)) %>% 
+        filter(work.balance.stars %in% c(0,0.5,1,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5)) %>%
+        filter(culture.values.stars %in% c(0,0.5,1,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5)) %>% 
+        filter(carrer.opportunities.stars %in% c(0,0.5,1,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5)) %>% 
+        filter(comp.benefit.stars %in% c(0,0.5,1,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5)) %>% 
+        filter(senior.mangemnet.stars %in% c(0,0.5,1,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5))
+    )
+      
     wc_data_sum = reactive({
       input$update
-      
       isolate({
-        
         withProgress({
           setProgress(message = "Processing corpus...")
+          getTermMatrix_sum(input$c_select)
         })
-        
       })
-      summary = data %>% select(company,summary) %>% filter(company == input$c_select)
-      corpus_sum = Corpus(VectorSource(summary$summary))
-      corpus_sum = tm_map(corpus_sum, content_transformer(tolower))
-      corpus_sum = tm_map(corpus_sum, removeNumbers)
-      corpus_sum = tm_map(corpus_sum, removeWords ,stopwords('english'))
-      corpus_sum = tm_map(corpus_sum, removePunctuation)
-      corpus_sum = tm_map(corpus_sum, stripWhitespace)
-      corpus_sum = tm_map(corpus_sum, removeWords,c("get","told","gave","took","can", "could"))
-      tdm_sum= TermDocumentMatrix(corpus_sum,control = list(minWordLength = 1))
-      m_sum= as.matrix(tdm_sum)
-      v_sum = sort(rowSums(m_sum), decreasing = TRUE)
-      d_sum = data.frame(word = names(v_sum), freq=v_sum)
     })
     
     wc_data_pro = reactive({
       input$update
-      
       isolate({
-        
         withProgress({
           setProgress(message = "Processing corpus...")
+          getTermMatrix_pro(input$c_select)
         })
-        
       })
-      
-      pro = data %>% select(company,pros) %>% filter(company == input$c_select)
-      corpus_pro = Corpus(VectorSource(pro$pros))
-      corpus_pro = tm_map(corpus_pro, content_transformer(tolower))
-      corpus_pro = tm_map(corpus_pro, removeNumbers)
-      corpus_pro = tm_map(corpus_pro, removeWords ,stopwords('english'))
-      corpus_pro = tm_map(corpus_pro, removePunctuation)
-      corpus_pro = tm_map(corpus_pro, stripWhitespace)
-      corpus_pro = tm_map(corpus_pro, removeWords,c("get","told","gave","took","can", "could"))
-      tdm_pro = TermDocumentMatrix(corpus_pro,control = list(minWordLength = 1))
-      m_pro = as.matrix(tdm_pro)
-      v_pro = sort(rowSums(m_pro), decreasing = TRUE)
-      d_pro = data.frame(word = names(v_pro), freq=v_pro)
-      
     })
-    
+
     wc_data_con = reactive({
       input$update
-      
       isolate({
-        
         withProgress({
           setProgress(message = "Processing corpus...")
+          getTermMatrix_con(input$c_select)
         })
-        
       })
-
-      
-      con = data %>% select(company,cons) %>% filter(company == input$c_select)
-      corpus_con = Corpus(VectorSource(con$cons))
-      corpus_con = tm_map(corpus_con, content_transformer(tolower))
-      corpus_con = tm_map(corpus_con, removeNumbers)
-      corpus_con = tm_map(corpus_con, removeWords ,stopwords('english'))
-      corpus_con = tm_map(corpus_con, removePunctuation)
-      corpus_con = tm_map(corpus_con, stripWhitespace)
-      corpus_con = tm_map(corpus_con, removeWords,c("get","told","gave","took","can", "could"))
-      tdm_con = TermDocumentMatrix(corpus_con,control = list(minWordLength = 1))
-      m_con = as.matrix(tdm_con)
-      v_con = sort(rowSums(m_con), decreasing = TRUE)
-      d_con = data.frame(word = names(v_con), freq=v_con)
-
     })
-    
+
     wc_data_advice = reactive({
       input$update
-      
       isolate({
-        
         withProgress({
           setProgress(message = "Processing corpus...")
+          getTermMatrix_advice(input$c_select)
         })
-        
       })
-      
-      advice.to.mgmt = data %>% select(company,advice.to.mgmt) %>% filter(company == input$c_select)
-      corpus_advice = Corpus(VectorSource(advice.to.mgmt$advice.to.mgmt))
-      corpus_advice = tm_map(corpus_advice, content_transformer(tolower))
-      corpus_advice = tm_map(corpus_advice, removeNumbers)
-      corpus_advice = tm_map(corpus_advice, removeWords ,stopwords('english'))
-      corpus_advice = tm_map(corpus_advice, removePunctuation)
-      corpus_advice = tm_map(corpus_advice, stripWhitespace)
-      corpus_advice = tm_map(corpus_advice, removeWords,c("get","told","gave","took","can", "could"))
-      tdm_advice = TermDocumentMatrix(corpus_advice, control = list(minWordLength = 1))
-      m_advice = as.matrix(tdm_advice)
-      v_advice = sort(rowSums(m_advice), decreasing = TRUE)
-      d_advice = data.frame(word = names(v_advice), freq=v_advice)
     })
     
 # Make the wordcloud drawing predictable during a session
     wordcloud_rep = repeatable(wordcloud)
-    
     output$wcplot_sum = renderPlot({
-      withProgress({
-        setProgress(message = "Creating Wordcloud...")
-        
+
       v1 = wc_data_sum()
-      wordcloud_rep(v1$word, v1$freq, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
-        
-      })
+      wordcloud_rep(names(v1), v1, rot.per = 0.3,min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
     })
-    
     output$wcplot_pro = renderPlot({
-
       v2 = wc_data_pro()
-      wordcloud_rep(v2$word, v2$freq, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
-      
+      wordcloud_rep(names(v2), v2, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
     })
-
     output$wcplot_con = renderPlot({
-
       v3 = wc_data_con()
-      wordcloud_rep(v3$word, v3$freq, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
-      
+      wordcloud_rep(names(v3), v3, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
     })
-
     output$wcplot_advice = renderPlot({
-
       v4 = wc_data_advice()
-      wordcloud_rep(v4$word, v4$freq, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
+      wordcloud_rep(names(v4), v4, rot.per = 0.3, min.freq = input$freq, max.words = input$max, colors = brewer.pal(8,"Dark2"))
     })
     
+    output$hist_rating <- renderPlot(
+      r_time_rating() %>%
+        filter(company == input$c_select) %>% 
+        ggplot(aes(x = get(input$Selection))) +
+        geom_histogram(binwidth = 0.5, bins = 10)
+      )
     
-    
-    
+    output$time_rating <- renderPlot(
+      r_time_rating() %>%
+        group_by(year, company) %>% 
+        summarise(avg = mean(get(input$Selection))) %>% 
+        ggplot(., aes(x = year, y = avg)) +
+        geom_line(aes(color = company))
+    )
     
     
     output$boxplot <- renderPlot(
--- ui.R
+++ ui.R
@@ -1,34 +1,30 @@
 
   
 
-ui= shinyUI(dashboardPage(
+shinyUI(dashboardPage(skin = "green",
   
-  dashboardHeader(title = "Reviews Exploration"),
+  dashboardHeader(title = "FAANG Reviews Exploration", titleWidth = 350),
 
   dashboardSidebar(
+    width = 350,
     sidebarMenu(
       sidebarMenu(
-        menuItem("General_Review", tabName = "General_Review", icon = icon("hist")),
-        menuItem("Rating_Review", tabName = "Selection", icon = icon("hist")),
-        menuItem("Wordcloud", tabName = "wordcloud", icon = icon("wordcloud"))
+        menuItem("General_Review", tabName = "General_Review", icon = icon("file-export")),
+        menuItem("Rating_Review", tabName = "Selection", icon = icon("bar-chart-o")),
+        selectizeInput(inputId = "Selection", label = "Select item to Display", choices = choice),
+        hr(),
+        menuItem("Wordcloud", tabName = "wordcloud", icon = icon("cloud-download-alt")),
+        selectizeInput(inputId = "c_select", label = "Choose a company",choices = companies),
+        actionButton("update", "Change"),
+        sliderInput(inputId = "freq", "Minimum Frequency:",min = 1, max = 50, value = 30),
+        sliderInput(inputId = "max", label = "Maximum Number of Words:",min = 1, max = 300, value = 250)
       ),
-      selectizeInput(inputId = "Selection",
-                     label = "Select item to Display",
-                     choices = choice),
-      selectizeInput(inputId = "c_select",
-                     label = "Choose a company",
-                     choices = companies),
-      actionButton("update", "Change"),
       hr(),
-      sliderInput(inputId = "freq",
-                  "Minimum Frequency:",
-                  min = 1, max = 50, value = 15),
-      sliderInput(inputId = "max",
-                  label = "Maximum Number of Words:",
-                  min = 1, max = 300, value = 100),
       sidebarMenu(
         menuItem("Source code", icon = icon("file-code-o"),href = "https://github.com/Andrlulu/Shiny_Project")
       )
+      
+      
     )
   ),
   
@@ -39,7 +35,9 @@ ui= shinyUI(dashboardPage(
       ),
       
       tabItem(tabName = "Selection",
-              box(plotOutput("boxplot"))
+              box(plotOutput("boxplot")),
+              box(plotOutput('time_rating')),
+              box(plotOutput('hist_rating'))
       ),
       
       tabItem(tabName = "wordcloud",
